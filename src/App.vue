6adae162a97e668633a8a1e0cf93e533c5dab9b1
Sen360 <54001936+Sen360@users.noreply.github.com>
2021-09-25 19:22:26 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Navbar />
+    <Body />
+    <Caroussel />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Caroussel from "./components/Caroussel.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    Body,
+    Caroussel,
+    Footer,
+  },
+};
+</script>
